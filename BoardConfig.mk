01695f10dc8bb5aeb69fcef17870911853302fc2
ArsenicRom <tumayordeseo5@hotmail.com>
2016-12-03 01:40:50 -0400

    fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,11 +75,11 @@ TARGET_KERNEL_CONFIG := lineageos_oneplus3_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9-kernel/bin
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+#KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9-kernel/bin
+#KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 
 # Optimizations
-STRICT_ALIASING := true
+STRICT_ALIASING := false
 CLANG_O3 := true
 ENABLE_GCCONLY := false
 GRAPHITE_OPTS := false
