d5ff39260e5a6d52c866904b791ec9d4a09cad52
ArsenicRom <tumayordeseo5@hotmail.com>
2018-02-28 01:15:21 -0400

    op3: Dex-preopt boot and system server
    WITH_DEXPREOPT_BOOT_IMG_ONLY was removed in 8.1 in favor of
    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY.
    
    Along with boot classpath it will also pre-optimize system server jars.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,12 +150,10 @@ TARGET_USES_QCOM_DISPLAY_BSP := true
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
+    WITH_DEXPREOPT ?= true
   endif
 endif
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(PLATFORM_PATH)/config.fs
